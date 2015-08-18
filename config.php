b4ebd99b495334e6adad2d7cbd1f83f8b68ea563
Alvaro Paçó <alvaro@scalasoft.com.br>
2015-08-17 22:56:46 -0300

    Setup

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencart/');
+define('HTTP_SERVER', 'http://localhost/opencart203/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/opencart/');
+define('HTTPS_SERVER', 'http://localhost/opencart203/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/wamp/www/opencart/catalog/');
-define('DIR_SYSTEM', 'C:/wamp/www/opencart/system/');
-define('DIR_LANGUAGE', 'C:/wamp/www/opencart/catalog/language/');
-define('DIR_TEMPLATE', 'C:/wamp/www/opencart/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/wamp/www/opencart/system/config/');
-define('DIR_IMAGE', 'C:/wamp/www/opencart/image/');
-define('DIR_CACHE', 'C:/wamp/www/opencart/system/cache/');
-define('DIR_DOWNLOAD', 'C:/wamp/www/opencart/system/download/');
-define('DIR_UPLOAD', 'C:/wamp/www/opencart/system/upload/');
-define('DIR_MODIFICATION', 'C:/wamp/www/opencart/system/modification/');
-define('DIR_LOGS', 'C:/wamp/www/opencart/system/logs/');
+define('DIR_APPLICATION', 'C:/wamp/www/opencart203/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/opencart203/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/opencart203/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/opencart203/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/opencart203/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/opencart203/image/');
+define('DIR_CACHE', 'C:/wamp/www/opencart203/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/opencart203/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/opencart203/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/opencart203/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/opencart203/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'opencart');
+define('DB_DATABASE', 'opencart203');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
