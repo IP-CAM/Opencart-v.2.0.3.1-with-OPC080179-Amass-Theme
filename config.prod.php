27b5235bceccd1232716ac45240639de4c510e35
Alvaro Paçó <alvaro@scalasoft.com.br>
2015-09-10 01:55:30 -0300

    Config de produção

---
+++ config.prod.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://goldsportsstore.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://goldsportsstore.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/home/scalasoft/webapps/lojagoldsports/catalog/');
+define('DIR_SYSTEM', '/home/scalasoft/webapps/lojagoldsports/system/');
+define('DIR_LANGUAGE', '/home/scalasoft/webapps/lojagoldsports/catalog/language/');
+define('DIR_TEMPLATE', '/home/scalasoft/webapps/lojagoldsports/catalog/view/theme/');
+define('DIR_CONFIG', '/home/scalasoft/webapps/lojagoldsports/system/config/');
+define('DIR_IMAGE', '/home/scalasoft/webapps/lojagoldsports/image/');
+define('DIR_CACHE', '/home/scalasoft/webapps/lojagoldsports/system/cache/');
+define('DIR_DOWNLOAD', '/home/scalasoft/webapps/lojagoldsports/system/download/');
+define('DIR_UPLOAD', '/home/scalasoft/webapps/lojagoldsports/system/upload/');
+define('DIR_MODIFICATION', '/home/scalasoft/webapps/lojagoldsports/system/modification/');
+define('DIR_LOGS', '/home/scalasoft/webapps/lojagoldsports/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'scala_gsports');
+define('DB_PASSWORD', '0051YGjxez40xDR5f063z4qtp');
+define('DB_DATABASE', 'scala_gsports');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
